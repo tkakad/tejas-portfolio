36b68eafd42a86c171b978a08cd0608dfe326161
Tejas Kakad <tejaskakad@Tejass-MacBook-Pro.local>
2023-10-06 12:17:05 -0500

    Completed styling and responsives for all sections except PROJECTS and CONTACT

---
+++ index.js
@@ -0,0 +1,33 @@
+function updateFooterLinks() {
+  const links = document.querySelectorAll('.footer-links');
+  
+  if (window.innerWidth <= 768) {
+      links.forEach(link => {
+          if (!link.hasAttribute('data-text')) {
+              link.setAttribute('data-text', link.textContent);
+          }
+          
+          if (link.getAttribute('data-text') === 'LinkedIn') {
+              link.innerHTML = '<i class="fa-brands fa-linkedin fa-xl"></i>';
+          } else if (link.getAttribute('data-text') === 'GitHub') {
+              link.innerHTML = '<i class="fa-brands fa-github fa-xl"></i>';
+          } else if (link.getAttribute('data-text') === 'Twitter') {
+              link.innerHTML = '<i class="fa-brands fa-twitter fa-xl"></i>';
+          } else if (link.getAttribute('data-text') === 'Resume') {
+              link.innerHTML = '<i class="fa-solid fa-file fa-xl"></i>';
+          }
+      });
+  } else {
+      links.forEach(link => {
+          if (link.hasAttribute('data-text')) {
+              link.innerHTML = link.getAttribute('data-text');
+          }
+      });
+  }
+}
+
+// Run the function on page load
+updateFooterLinks();
+
+// Monitor window resize event
+window.addEventListener('resize', updateFooterLinks);
